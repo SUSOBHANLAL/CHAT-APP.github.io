0e1f4de2c37a0dda49543afd5a2a183426ddc47f
SUSOBHAN LAL <115396834+SUSOBHANLAL@users.noreply.github.com>
2023-07-23 17:24:33 +0530

    Update chatSlice.js

--- src/store/chatSlice.js
+++ src/store/chatSlice.js
@@ -5,37 +5,31 @@ const chatSlice = createSlice({
     initialState: {},
     reducers: {
         add(state, action) {
-            // state.push(action.payload);
+            
             return {...state, [0]: action.payload}
-            // return {[0]: action.payload};
+           
         },
         remove(state, action) {
             return state.filter((item) => item.id !== action.payload);
         },
         addID(state, action) {
-        //    return state[0].chatID(action.payload);
-        //    return state.filter((item) => {
-        //     item.chatID = action.payload;
-        //    });
+       
         
-        return { ...state, [1]: action.payload } // correct
-        // return { ...state, state[1].chatID: action.payload }
-        // return { chatID: action.payload, ...state  }
+        return { ...state, [1]: action.payload } 
         
-        // state.push(action.payload);
         },
         addChatLog(state, action) {
             let newState = state;
             let tempID = state[1].chatID; 
-            // console.log('tempid',tempID);
+          
             let newFriends = newState[0].friends;
-            // console.log(newFriends);
+         
             console.log('a',newFriends.length);
             for(let i =0;i<newFriends.length;i++){
-                // console.log('a',newFriends[i].id);
+               
                 if(newFriends[i].id == tempID){
                     newFriends[i].chatlog.push(action.payload);
-                    // console.log('ok');
+                   
                 }
             }
             return newState;
