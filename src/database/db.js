fa4593df9e286ab2424c176236e84d11f53644ef
susobhan lal <susobhanlal2017gmail.comgit config --global user.password Babamaami90>
2023-07-23 16:43:15 +0530

    first commit

---
+++ src/database/db.js
@@ -0,0 +1,175 @@
+const file ={
+  "chatID": 3,
+  "profile": {
+    "id": 1,
+    "name": "MUSKAN",
+    "picture": "https://cdn.pixabay.com/photo/2019/02/06/14/21/cat-3979262_1280.jpg",
+    "status": "Available",
+    "friends": [
+      {
+        "id": 2,
+        "name": "PRIYANKA",
+        "picture": "https://cdn.pixabay.com/photo/2019/01/31/03/09/fantasy-3965864_1280.jpg",
+        "latest_timestamp": "10:00",
+        "lastChat":
+          "Or maybe not, let me check logistics and call you. Give me sometime"
+      },
+      {
+        "id": 3,
+        "name": "RIPSITA",
+        "picture": "https://cdn.pixabay.com/photo/2013/07/13/12/47/girl-160326_1280.png",
+        "latest_timestamp": "12:30",
+        "lastChat": "Alright"
+      },
+      {
+        "id": 4,
+        "name": "DILIP",
+        "picture": " https://cdn.pixabay.com/photo/2014/12/22/00/07/tree-576823_1280.png",
+        "latest_timestamp": "4:12",
+        "lastChat": "I will look into it"
+      },
+      {
+        "id": 5,
+        "name": "KALPANA",
+        "picture": "https://cdn.pixabay.com/photo/2014/12/22/00/07/tree-576820_1280.png",
+        "latest_timestamp": "7:53",
+        "lastChat": "Exactly my point!"
+      },
+      {
+        "id": 6,
+        "name": "RUJUTHA",
+        "picture": "https://cdn.pixabay.com/photo/2018/09/11/14/49/moe-3669736_1280.png",
+        "latest_timestamp": "1:09",
+        "lastChat": "Not quite the same."
+      },
+     
+    ]
+  },
+  "friends": [
+    {
+      "id": 2,
+      "name": "PRIYANKA",
+      "picture": "https://cdn.pixabay.com/photo/2019/01/31/03/09/fantasy-3965864_1280.jpg",
+      "chatlog": [
+        {
+          "text": "Hii?",
+          "timestamp": "10:00",
+          "side": "left",
+          "message_id": 1
+        },
+        {
+          "text":
+            "HII Priyanka",
+          "timestamp": "10:05",
+          "side": "left",
+          "message_id": 5
+        }
+        
+      ]
+    },
+    {
+      "id": 3,
+      "name": "RIPSITA",
+      "picture": " https://cdn.pixabay.com/photo/2013/07/13/12/47/girl-160326_1280.png",
+      "chatlog": [
+        {
+          "text":
+            "Are you wants to join coding ninja",
+          "timestamp": "06:55",
+          "side": "left",
+          "message_id": 1
+        },
+        {
+          "text":
+            "hi ninja",
+          "timestamp": "07:05",
+          "side": "left",
+          "message_id": 4
+        },
+      ]
+    },
+    {
+      "id": 5,
+      "name": "KALPANA",
+      "picture": "https://cdn.pixabay.com/photo/2014/12/22/00/07/tree-576820_1280.png",
+      "chatlog": [
+        {
+        "text": "Hi",
+        "timestamp": "07:05",
+        },
+        {
+          "text": "How are you",
+          "timestamp": "07:06",
+          }
+      ]
+    },
+    {
+      "id": 6,
+      "name": "RUJUTHA",
+      "picture": "https://cdn.pixabay.com/photo/2018/09/11/14/49/moe-3669736_1280.png",
+      "chatlog": []
+    },
+    {
+      "id": 7,
+      "name": "PREETHI",
+      "picture": "https://cdn.pixabay.com/photo/2022/11/26/19/50/kid-7618343_1280.jpg",
+      "chatlog": []
+    },
+    
+    {
+      "id": 15,
+      "name": "TRIPTI",
+      "picture": " https://cdn.pixabay.com/photo/2018/03/31/03/46/kitten-3277268_1280.png",
+      "chatlog": []
+    }
+  ],
+  "allFriends": [
+  
+    {
+      "id": 15,
+      "name": "TRIPTI",
+      "picture": " https://cdn.pixabay.com/photo/2018/03/31/03/46/kitten-3277268_1280.png",
+      "status": "Busy"
+    },
+    {
+      "id": 7,
+      "name": "PREETHI",
+      "picture": "https://cdn.pixabay.com/photo/2022/11/26/19/50/kid-7618343_1280.jpg",
+      "status": "Busy"
+    },
+    {
+      "id": 6,
+      "name": "RUJUTHA",
+      "picture": " https://cdn.pixabay.com/photo/2018/09/11/14/49/moe-3669736_1280.png",
+      "status": "Busy"
+    },
+    {
+      "id": 5,
+      "name": "KALPANA",
+      "picture": "https://cdn.pixabay.com/photo/2014/12/22/00/07/tree-576820_1280.png",
+      "status": "Busy"
+    },
+    {
+      "id": 4,
+      "name": "DILIP",
+      "picture": "https://cdn.pixabay.com/photo/2014/12/22/00/07/tree-576823_1280.png",
+      "status": "Busy"
+    },
+    {
+      "id": 3,
+      "name": "RIPSITA",
+      "picture": "https://cdn.pixabay.com/photo/2013/07/13/12/47/girl-160326_1280.png",
+      "status": "Busy"
+    },
+    {
+      "id": 2,
+      "name": "PRIYANKA",
+      // "picture": "https://i.ibb.co/jTQmFdc/sandeep.jpg",
+      "picture": "https://cdn.pixabay.com/photo/2019/01/31/03/09/fantasy-3965864_1280.jpg",
+      "status": "Busy"
+    },
+   
+  ]
+}
+
+export default file;
